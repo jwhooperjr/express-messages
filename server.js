40bcc6a2a23a5a66f7f260526bd814646b2961b3
John Hooper <jwhooperjr@gmail.com>
2020-03-06 10:36:08 -0700

    Set up routes

--- server.js
+++ server.js
@@ -5,13 +5,38 @@ const app = express();
 // const Message = require('./db/Message');
 
 // if you are using postgres, uncomment this line
-// const pool = require('./db/pgconfig');
+const pool = require('./db/pgconfig');
 
 app.use((req,res,next) => {
   res.status(404).send('That route does not exist');
 });
 
 const port = 3000;
+
+app.post('/messages', (req, res, next) => {
+  pool.postAMessage()
+  .then(() => res.sendStatus(201))
+});
+
+app.get('/messages', (req, res, next) => {
+  pool.getAllMessages()
+  .then(results => res.send(results))
+});
+
+app.put('/messages/:id', (req, res, next) => {
+  pool.updateMessage()
+  .then()
+});
+
+app.delete('/messages/:id', (req, res, next) => {
+  pool.deleteMessage()
+  .then()
+});
+
+app.get('/messages/:id', (req, res, next) => {
+  pgconfig.getAMessage()
+  .then(results => res.send(results))
+});
 
 app.listen(port, () => {
   console.log('Listening on port', port);
