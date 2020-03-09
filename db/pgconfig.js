483fa698aa384727f67017cbf13ad6593a9f39e0
John Hooper <jwhooperjr@gmail.com>
2020-03-09 07:46:57 -0600

    Implement models for post and get

--- db/pgconfig.js
+++ db/pgconfig.js
@@ -2,17 +2,25 @@ const { Pool } = require('pg');
 const pool = new Pool({
   database: 'messages'
 });
-
+// pool.query('select * from messages').then(res => {console.log(res.rows)});
 const postAMessage = (message) => {
-
+  let values = [message.message_id, message.user_name, message.message_body]
+  return pool.query('INSERT INTO messages(message_id, user_name, message_body) VALUES($1, $2, $3)', values)
+  .then(() => true)
 }
 
 const getAllMessages = () => {
-
+  return pool.query('select * from messages')
+  .then((response) => {
+    return response.rows;
+  }).catch((err) => {
+    return err;
+  })
 }
 
 const updateMessage = (message) => {
-
+  return pool.query('update messages set message_body = \'blah\' where message_id = 1')
+  .then(() => true)
 }
 
 const deleteMessage = (message) => {
@@ -20,6 +28,12 @@ const deleteMessage = (message) => {
 }
 
 const getAMessage = (message) => {
-  
+
 }
-module.exports = pool;
+module.exports = {
+  postAMessage,
+  getAllMessages,
+  updateMessage,
+  deleteMessage,
+  getAMessage
+};
