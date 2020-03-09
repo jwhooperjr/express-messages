23c473e5cfadf4c1080bbc8d52e1d9da24ec44a9
John Hooper <jwhooperjr@gmail.com>
2020-03-09 07:47:35 -0600

    Import model functions to use in routes, include bodyParser

--- server.js
+++ server.js
@@ -1,47 +1,53 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
+app.use(bodyParser());
 
 // if you are using mongo/mongoose uncomment this line
 // const Message = require('./db/Message');
 
 // if you are using postgres, uncomment this line
-const pool = require('./db/pgconfig');
+const { postAMessage, getAllMessages, updateMessage, deleteMessage, getAMessage } = require('./db/pgconfig');
 
 const port = 3000;
 
 app.post('/messages', (req, res) => {
-  pool.postAMessage(req.body)
-  .then(results => res.send(results))
-  .catch(err => res.send(err))
+  postAMessage(req.body).then((results) => {
+  res.send(results);
+  })
+  // console.log(JSON.stringify(req.body));
+  // res.send('hello');
 });
 
 app.get('/messages', (req, res) => {
-  pool.getAllMessages()
-  .then(results => res.send(results))
-  .catch(err => res.send(err))
+  getAllMessages().then((results) => {
+    res.send(results);
+  })
 });
 
 app.put('/messages/:id', (req, res) => {
-  pool.updateMessage(req.body)
-  .then(results => res.send(results))
-  .catch(err => res.send(err))
+  updateMessage(req.body).then((results) => {
+    res.send(results)
+  })
+  // res.send(req.body);
+  // console.log(req.body);
 });
 
-app.delete('/messages/:id', (req, res) => {
-  pool.deleteMessage()
-  .then(results => res.send(results))
-  .catch(err => res.send(err))
-});
+// app.delete('/messages/:id', (req, res) => {
+//   pool.deleteMessage()
+//   .then(results => res.send(results))
+//   .catch(err => res.send(err))
+// });
 
-app.get('/messages/:id', (req, res) => {
-  pool.getAMessage(req.params.id)
-  .then(results => res.send(results))
-  .catch(err => res.send(err))
-});
+// app.get('/messages/:id', (req, res) => {
+//   pool.getAMessage(req.params.id)
+//   .then(results => res.send(results))
+//   .catch(err => res.send(err))
+// });
 
-app.use((req,res,next) => {
-  res.status(404).send('That route does not exist');
-});
+// app.use((req,res,next) => {
+//   res.status(404).send('That route does not exist');
+// });
 
 app.listen(port, () => {
   console.log('Listening on port', port);
